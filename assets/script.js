84b37b3e34ff8e79c0900db176152aab258ba66c
Peter Jolley <peterjolley456@gmail.com>
2022-03-25 12:41:26 +1030

    Added variables and text boxes for client to chose password

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,49 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   
-  var passwordText = document.querySelector("#password");
-  var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890  !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-  var minimumLength = 8;
-  var maximumLength = 128;
-  var passwordLength;
-  var password = "";
+  const passwordText = document.querySelector("#password");
+  //const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%&'()*+,-./\\:;<=>?@][^_`{|}~";
+  let characters = "";
+  let lowercaseCharachters = false;
+  let uppercase = false;
+  let numeric = false;
+  let special = false;
+  while(!lowercaseCharachters && !uppercase && !numeric && !special) {
+    lowercaseCharachters = confirm("Do you want lowercase charachters?");
+    uppercase =  confirm("Do you want upeprcase charachters?");
+    numeric =  confirm("Do you want numeric charachters");
+    special =  confirm("Do you want special charachters");
+    if(!lowercaseCharachters && !uppercase && !numeric && !special) {
+      alert("Must select at least one character type")
+    }
+
+  }
+
+  if (lowercaseCharachters) {
+    characters += "abcdefghijklmnopqrstuvwxyz"
+  }
+  if (uppercase) {
+    characters += "ABCDEFGHIJKLMNOPQRSTUVWXY"
+  }if (numeric) {
+    characters += "1234567890"
+  }if (special) {
+    characters += "!#$%&'()*+,-./\\:;<=>?@][^_`{|}~\""
+  }
+
+
+  const minimumLength = 8;
+  const maximumLength = 128;
+  let passwordLength = prompt("type the length of password") ;
+  let password = "";
   passwordText.value = password;
-  for(var i = 0; i <= passwordLength; i++){
-    var randomCharacter =Math.floor(Math.random() * characters.length);
+  for(let i = 0; i <= passwordLength; i++){
+    const randomCharacter =Math.floor(Math.random() * characters.length);
     password += characters.substring(randomCharacter, randomCharacter +1);
   }
-  document.getElementById("password").value = password;
+  passwordText.value = password;
 }
     
 
