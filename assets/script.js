45b7d69ec965be89c2e89e3c5db0285225c6219b
Peter Jolley <peterjolley456@gmail.com>
2022-03-25 11:45:50 +1030

    attempt to create password

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,23 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  
   var passwordText = document.querySelector("#password");
-
+  var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890  !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  var minimumLength = 8;
+  var maximumLength = 128;
+  var passwordLength;
+  var password = "";
   passwordText.value = password;
-
+  for(var i = 0; i <= passwordLength; i++){
+    var randomCharacter =Math.floor(Math.random() * characters.length);
+    password += characters.substring(randomCharacter, randomCharacter +1);
+  }
+  document.getElementById("password").value = password;
 }
+    
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
